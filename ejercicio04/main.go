978dbf9d004a71d879ff640920f093d33ed5f68a
carlos guzman <carlosguzmanbaquero@gmail.com>
2021-03-10 15:04:44 -0500

    sentencia if y captura de teclado

---
+++ ejercicio04/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//declaracion de variables globales
+var numero1 int
+var numero2 int
+var resultado int
+var leyenda string
+
+func main() {
+	fmt.Println("Ingrese numero 1: ")
+	//fmt.Scanf("%d", &numero1)
+	fmt.Scanln(&numero1)
+
+	fmt.Println("Ingrese numero 2: ")
+	//fmt.Scanf("%d", &numero2)
+	fmt.Scanln(&numero2)
+
+	fmt.Println("Descripcion: ")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		leyenda = scanner.Text()
+	}
+
+	resultado = numero1 + numero2
+	fmt.Println(leyenda, resultado)
+
+}
